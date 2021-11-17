9904ff79acb7ac29d110f40b48ac6b39dd62d047
Hooseinsalari <hossein.salari002@gmail.com>
2021-11-17 13:47:48 +0330

    add a input for search transaction

--- my-app/src/components/TransActionsComponent.js
+++ my-app/src/components/TransActionsComponent.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const TransActionsComponent = ({ transActions }) => {
+  const [searchItem, setSearchItem] = useState("")
+  const [filteredTnx, setFilteredTnx] = useState(transActions)
+
+  const filteredTransactions = (search) => {
+    if(!search || search === ""){
+      setFilteredTnx(transActions)
+      return
+    }
+    const filter = transActions.filter((item) => item.desc.toLowerCase().includes(search.toLowerCase()))
+    setFilteredTnx(filter)
+  }
+
+  const changeHandler = (event) => {
+    setSearchItem(event.target.value);
+    filteredTransactions(event.target.value)
+  }
+
+  useEffect(() => {
+    filteredTransactions(searchItem);
+  }, [transActions])
+
   return (
     <div>
-      {transActions.map((item) => (
+      
+      <input className="valueInput" type="text" value={searchItem} onChange={changeHandler} placeholder="search your transactions" />
+      {filteredTnx.map((item) => (
         <div className={`result ${item.type === `Expense` ? `resultExpense` : `resultIncome`}`} key={item.id}>
           <p className="descResult">{item.desc}</p>
           <div className="twoValue">
